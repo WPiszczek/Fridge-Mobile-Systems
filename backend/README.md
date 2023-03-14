3957928583731adf70f55763f615f6eb46f01b89
WPiszczek <wojciech.piszczek@student.put.poznan.pl>
2023-03-14 10:22:38 +0100

    add readme

---
+++ backend/README.md
@@ -0,0 +1,18 @@
+## 1. Diagram encji
+
+![Alt diagram encji](./entity_diagram.png)
+
+## 2. API
+
+POST /api/auth/login  
+POST /api/auth/register  
+POST /api/auth/google  
+
+GET /api/{user_id}/products  
+GET /api/{user_id}/products/{product_id}  
+POST /api/{user_id}/products  
+PUT /api/{user_id}/products/{product_id}  
+PATCH /api/{user_id}/products/{product_id}  
+DELETE /api/{user_id}/products/{product_id}  
+
+GET /api/{user_id}/stats
--- backend/index.ts
+++ backend/index.ts
@@ -1,15 +1,16 @@
-import express, { Express, Request, Response } from 'express';
-import dotenv from 'dotenv';
+import express, { Express, Request, Response } from "express";
+import dotenv from "dotenv";
 
 dotenv.config();
 
 const app: Express = express();
 const port = process.env.PORT;
+const host_url = process.env.HOST_URL;
 
-app.get('/', (request: Request, response: Response) => {
-  response.send('ok');
+app.get("/", (request: Request, response: Response) => {
+  response.send("ok");
 });
 
 app.listen(port, () => {
-  console.log(`[server]: Server is running at http://localhost:${port}`);
+  console.log(`[server]: Server is running at ${host_url}:${port}`);
 });
