80be15e38dde0b713f861c7a33a102bd226dc81d
Filip Sauer <sauer.filip@gmail.com>
2023-05-10 09:35:15 +0200

    mobile: add modal to barcodeScanner screen

--- mobile/app/(tabs)/barcodeScanner.tsx
+++ mobile/app/(tabs)/barcodeScanner.tsx
@@ -1,12 +1,13 @@
 import { BarCodeScannedCallback, BarCodeScanner } from "expo-barcode-scanner";
 import { useEffect, useState } from "react";
-import { StyleSheet } from "react-native";
+import { Alert, Modal, Pressable, StyleSheet } from "react-native";
 
 import { Text, View } from "../../components/Themed";
 
 export default function BarcodeScannerScreen() {
   const [hasPermission, setHasPermission] = useState<boolean | null>(null);
   const [barCode, setBarCode] = useState<string>();
+  const [modalVisible, setModalVisible] = useState(false);
 
   useEffect(() => {
     const getBarCodeScannerPermissions = async () => {
@@ -37,6 +38,31 @@ export default function BarcodeScannerScreen() {
         onBarCodeScanned={handleBarCodeScanned}
         style={StyleSheet.absoluteFillObject}
       />
+      <Pressable onPress={() => setModalVisible(true)}>
+        <Text>Open the modal</Text>
+      </Pressable>
+
+      <Modal
+        animationType="slide"
+        transparent={true}
+        visible={modalVisible}
+        onRequestClose={() => {
+          Alert.alert("Modal has been closed.");
+          setModalVisible(!modalVisible);
+        }}
+      >
+        <View style={styles.centeredView}>
+          <View style={styles.modalView}>
+            <Text style={styles.modalText}>Hello World!</Text>
+            <Pressable
+              style={[styles.button, styles.buttonClose]}
+              onPress={() => setModalVisible(!modalVisible)}
+            >
+              <Text style={styles.textStyle}>Hide Modal</Text>
+            </Pressable>
+          </View>
+        </View>
+      </Modal>
       {barCode && <Text>{barCode}</Text>}
     </View>
   );
@@ -56,5 +82,47 @@ const styles = StyleSheet.create({
     marginVertical: 30,
     height: 1,
     width: "80%",
+  },
+  centeredView: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    marginTop: 22,
+    backgroundColor: "transparent",
+  },
+  modalView: {
+    margin: 20,
+    backgroundColor: "white",
+    borderRadius: 20,
+    padding: 35,
+    alignItems: "center",
+    shadowColor: "#000",
+    shadowOffset: {
+      width: 0,
+      height: 2,
+    },
+    shadowOpacity: 0.25,
+    shadowRadius: 4,
+    elevation: 5,
+  },
+  button: {
+    borderRadius: 20,
+    padding: 10,
+    elevation: 2,
+  },
+  buttonOpen: {
+    backgroundColor: "#F194FF",
+  },
+  buttonClose: {
+    backgroundColor: "#2196F3",
+  },
+  textStyle: {
+    color: "white",
+    fontWeight: "bold",
+    textAlign: "center",
+  },
+  modalText: {
+    marginBottom: 15,
+    textAlign: "center",
   },
 });
