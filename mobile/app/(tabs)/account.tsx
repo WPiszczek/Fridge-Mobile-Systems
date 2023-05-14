dc51d8d7f4b1a1b147cdd37956d810d033b00c92
Filip Sauer <sauer.filip@gmail.com>
2023-05-14 23:41:54 +0200

    mobile: add register and login forms

--- mobile/app/(tabs)/account.tsx
+++ mobile/app/(tabs)/account.tsx
@@ -1,12 +1,36 @@
-import { StyleSheet } from 'react-native';
+import { StyleSheet, Text, Button } from "react-native";
 
-import { Input } from '../../components/Input';
-import { View } from '../../components/Themed';
+import { Input } from "../../components/Input";
+import { View } from "../../components/Themed";
+import { useCallback } from "react";
 
 export default function TabThreeScreen() {
+  const handleLogin = useCallback(async () => {
+    const res = await fetch("http://localhost:8000/api/products");
+    const data = await res.json();
+    alert(JSON.stringify(data, null, 2));
+  }, []);
+
   return (
     <View style={styles.container}>
-      <Input>test</Input>
+      <Text>Register</Text>
+      <Input placeholder="login" />
+      <Input placeholder="password" textContentType="newPassword" />
+      <Input placeholder="repeat password" textContentType="newPassword" />
+      <Button
+        onPress={() => alert("hello")}
+        title="Register"
+        accessibilityLabel="Learn more about this purple button"
+      />
+
+      <Text>Login</Text>
+      <Input placeholder="login" />
+      <Input placeholder="password" textContentType="password" />
+      <Button
+        onPress={handleLogin}
+        title="Login"
+        accessibilityLabel="Learn more about this purple button"
+      />
     </View>
   );
 }
@@ -14,16 +38,17 @@ export default function TabThreeScreen() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
+    alignItems: "center",
+    padding: 15,
+    gap: 10,
   },
   title: {
     fontSize: 20,
-    fontWeight: 'bold',
+    fontWeight: "bold",
   },
   separator: {
     marginVertical: 30,
     height: 1,
-    width: '80%',
+    width: "80%",
   },
 });
