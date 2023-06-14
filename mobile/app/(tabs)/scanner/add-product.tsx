9c75f5709ec8a0a5ca4d9fad4b7a52bd9373c749
Filip Sauer <sauer.filip@gmail.com>
2023-06-14 08:22:32 +0200

    source product name on product fetch

--- mobile/app/(tabs)/scanner/add-product.tsx
+++ mobile/app/(tabs)/scanner/add-product.tsx
@@ -1,40 +1,57 @@
 import { useQuery } from "@tanstack/react-query";
 import axios from "axios";
-import { useLocalSearchParams } from "expo-router";
+import { useLocalSearchParams, useNavigation } from "expo-router";
 import { Text } from "react-native";
-import { FormInput } from "../../../components/FormInput";
 import { useForm } from "react-hook-form";
+import { PaperFormInput } from "../../../components/PaperFormInput";
+import { useEffect } from "react";
+
+interface Product {
+  product?: {
+    product_name: string;
+  };
+}
 
 const AddProduct = () => {
+  const navigation = useNavigation();
   const { barcode } = useLocalSearchParams<{ barcode: string }>();
 
   const { data, error, isLoading, isFetching } = useQuery(
     ["product", barcode],
     ({ queryKey: [, barcode] }) =>
-      axios.get(
+      axios.get<Product>(
         `https://world.openfoodfacts.org/api/v0/product/${barcode}.json`
       ),
     {
       enabled: !!barcode && (barcode.length === 8 || barcode.length === 13),
+      select: (data) => data.data,
     }
   );
 
-  const { control } = useForm({ defaultValues: { productCode: barcode } });
+  const { control, setValue } = useForm({
+    defaultValues: { productCode: barcode, productName: "" },
+  });
 
-  // if (isLoading)
-  //   return (
-  //     <Text>
-  //       Loading {barcode}...
-  //     </Text>
-  //   );
+  useEffect(() => {
+    setValue("productName", data?.product?.product_name ?? "");
+  }, [data]);
 
   return (
     <>
-      <FormInput
+      <PaperFormInput
         control={control}
         name="productCode"
         placeholder="product code"
         keyboardType="numeric"
+        onChange={(e) => {
+          // @ts-expect-error there's no way to type it
+          navigation.setParams({ barcode: e.nativeEvent.text });
+        }}
+      />
+      <PaperFormInput
+        control={control}
+        name="productName"
+        placeholder="product name"
       />
       <Text>{JSON.stringify(data, null, 2)}</Text>
     </>
